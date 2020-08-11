9279cfb1bc5ef1b38e07c7bebf50ac2a9f558f22
Freeman535 <59725156+Freeman535@users.noreply.github.com>
2020-08-11 10:23:44 +0800

    Update and rename xlsx_to_json.py to xls_to_json.py

--- tools/xls_to_json.py
+++ tools/xls_to_json.py
@@ -10,34 +10,28 @@ def get_desktop():  # 获取桌面地址
 
 def read_x():  # 读取表格
     desktop = get_desktop()
-    data = xlrd.open_workbook(str(desktop) + '\data_json_format.xlsx')
+    data = xlrd.open_workbook(str(desktop) + '\out.xls')
     table = data.sheets()[0]
-    rows = table.nrows
-    data = []
+    rows = table.nrows  # 行
+    cols = table.ncols  # 列
+    h = []
+    data = '['
+    for y in range(cols):
+        h.append(str(table.row_values(0)[y]))
+    print(h)
     for x in range(1, rows):
-        values = table.row_values(x)
-        data.append(
-            (
-                {
-                    "sp_code": str(values[0]),
-                    "code": str(values[1]),
-                    "wl_code": str(values[2]),
-                    "sp_gg": str(values[3]),
-                    "gg": str(values[4]),
-                    "name": str(values[5]),
-                    "jhj": str(values[6]),
-                    "sj": str(values[7]),
-                    "sj-hd": str(values[8]),
-                    "class1": str(values[9]),
-                    "class2": str(values[10]),
-                    "class3": str(values[11])
-                }
-            )
-        )
+        data2 = ''
+        for xy in range(cols):
+            values = table.row_values(x)
+            data2 = data2 + '"' + h[xy] + '": "' + str(values[xy]) + '", '
+        data2 = '{' + data2[:-2] + '}, '
+        data = data + data2
+    data = data[:-2] + ']'
     return data
 
 
 if __name__ == '__main__':
+    print('把需要转换为json的xls文件放到桌面，文件名改为out.xls')
     time_c = time.strftime("%Y-%m-%d", time.localtime(time.time()))  # 获取本地日期
     author = input("今天是" + str(time_c) + "\n输入作者名")  # 获取作者
     desktop = get_desktop()
